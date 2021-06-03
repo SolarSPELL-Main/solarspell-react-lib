5652c801f1a8086122089ae38ceca565f3e9d6b1
Jonathan Zhao <jjzhao@asu.edu>
2021-06-03 16:58:43 -0700

    Added ExpansionPanel + DataGrid display

--- src/components/MetadataTable.tsx
+++ src/components/MetadataTable.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import { DataGrid, GridColDef } from '@material-ui/data-grid';
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
 
 interface MetadataTableProps {
-  metadataTypes: SerializedMetadataType[]
+  metadataType: SerializedMetadataType
   metadata: SerializedMetadata[]
 }
 
+const columns: GridColDef[] = [
+  {
+    field: 'name',
+    headerName: 'Metadata Name',
+    flex: 1,
+  },
+];
+
+const accordionHeaderStyle: React.CSSProperties = {
+  fontWeight: 600,
+};
+
 function MetadataTable(props: MetadataTableProps): React.ReactElement {
   return (
-    <h1>Hello, world!</h1>
-  )
+    <Accordion>
+      <AccordionSummary>
+        <Typography style={accordionHeaderStyle}>{props.metadataType.name}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <DataGrid columns={columns} rows={props.metadata} autoHeight />
+      </AccordionDetails>
+    </Accordion>
+  );
 }
 
 export default MetadataTable;
