b43af6629b4ba276b4647da6795617becfcdc038
Jonathan Zhao <jjzhao@asu.edu>
2021-06-03 17:47:22 -0700

    Added multi-metadata type component

---
+++ src/components/MetadataDisplay.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import MetadataTable from './MetadataTable';
+
+interface MetadataDisplayProps {
+  metadataTypes: SerializedMetadataType[]
+  metadata: Record<string, SerializedMetadata[]>
+}
+
+/**
+ * This component creates multiple expandable tabs containg tables for each metadata type.
+ * Only the types in metadataTypes will be displayed, even if metadata contains additional types.
+ * @param props The data for the tables.
+ * @returns A series of expandable panels containing the metadata in tables.
+ */
+function MetadataDisplay(props: MetadataDisplayProps): React.ReactElement {
+  return (
+    <>
+      {props.metadataTypes.map(metadataType => {
+        return (<MetadataTable metadataType={metadataType} metadata={props.metadata[metadataType.name]} />);
+      })}
+    </>
+  );
+}
+
+export default MetadataDisplay;
