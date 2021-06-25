479d74e9ee9fa1aba76e02f12b5b59ae4dfab1f8
Jonathan Zhao <zhaojonathan99@gmail.com>
2021-06-25 12:59:35 -0700

    Refactored typings + propFactory, raised modal items above

--- example/src/MockContentModal.tsx
+++ example/src/MockContentModal.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { ContentModal } from 'solarspell-react-lib';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Checkbox from '@material-ui/core/Checkbox';
+import { KeyboardDatePicker } from '@material-ui/pickers';
+import { ContentModal, BaseContent } from 'solarspell-react-lib';
+
+type FullBaseContent = {
+  duplicatable: boolean
+  notes: string
+  reviewDate: Date
+  file: File
+} & BaseContent
 
 function MockContentModal(): React.ReactElement {
   const [open, setOpen] = React.useState(false);
@@ -21,8 +32,185 @@ function MockContentModal(): React.ReactElement {
     >
       Add Content
     </Button>
-    <ContentModal
-      items={[]}
+    <ContentModal<FullBaseContent>
+      items={[
+        {
+          component: TextField,
+          propFactory: (_s, reasons, setter) => {
+            return {
+              fullWidth: true,
+              label: 'Title',
+              onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
+                setter(event.currentTarget.value);
+              },
+              error: !!reasons['title'],
+              helperText: reasons['title'],
+            };
+          },
+          label: 'title',
+          initialValue: '',
+          validator: (state) => {
+            if (!state['title']) {
+              return 'Title is required.';
+            } else {
+              return null;
+            }
+          },
+        },
+        {
+          component: TextField,
+          propFactory: (_s, _r, setter) => {
+            return {
+              fullWidth: true,
+              label: 'Description',
+              onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
+                setter(event.currentTarget.value);
+              },
+            };
+          },
+          label: 'description',
+          initialValue: '',
+        },
+        {
+          component: (props) => (
+            <>
+              <Button
+                variant={'contained'}
+                component={'label'}
+                onChange={props.onChange}
+              >
+                Upload File
+                <input
+                  type={'file'}
+                  accept={'*'}
+                  hidden
+                />
+              </Button>
+              <Typography>{props.text}</Typography>
+            </>
+          ),
+          propFactory: (state, _r, setter, genericSetter) => {
+            return {
+              onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
+                const target = event.target as HTMLInputElement;
+                const file = target.files?.[0];
+                if (file) {
+                  setter(file);
+                  genericSetter('fileName', file.name);
+                }
+              },
+              text: state['fileName'] ?
+                `Existing file: ${state['fileName']}`
+                :
+                'No file chosen',
+            };
+          },
+          label: 'file',
+          initialValue: '',
+        },
+        {
+          component: TextField,
+          propFactory: (_s, reasons, setter) => {
+            return {
+              fullWidth: true,
+              label: 'Year of Publication',
+              onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
+                setter(event.currentTarget.value);
+              },
+              error: !!reasons['datePublished'],
+              helperText: reasons['datePublished'],
+            };
+          },
+          label: 'datePublished',
+          initialValue: '',
+          validator: (state) => {
+            if (!state['datePublished'] || isNaN(Number(state['datePublished']))) {
+              return 'Invalid year';
+            } else {
+              return null;
+            }
+          },
+        },
+        {
+          component: KeyboardDatePicker,
+          propFactory: (state, _r, setter) => {
+            return {
+              disableToolbar: true,
+              variant: 'inline',
+              format: 'MM/dd/yyyy',
+              label: 'Reviewed Date',
+              onChange: (date: Date) => {
+                setter(date);
+              },
+              value: state['reviewDate'],
+            };
+          },
+          label: 'reviewDate',
+          initialValue: new Date(),
+        },
+        {
+          component: TextField,
+          propFactory: (_s, _r, setter) => {
+            return {
+              fullWidth: true,
+              label: 'Copyright Notes',
+              onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
+                setter(event.currentTarget.value);
+              },
+            };
+          },
+          label: 'copyright',
+          initialValue: '',
+        },
+        {
+          component: TextField,
+          propFactory: (_s, _r, setter) => {
+            return {
+              fullWidth: true,
+              label: 'Rights Statement',
+              onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
+                setter(event.currentTarget.value);
+              },
+            };
+          },
+          label: 'rightsStatement',
+          initialValue: '',
+        },
+        {
+          component: (props) => {
+            return (
+              <>
+                <Typography>Duplicatable</Typography>
+                <Checkbox {...props} />
+              </>
+            );
+          },
+          propFactory: (state, _r, setter) => {
+            return {
+              checked: state['duplicatable'],
+              onChange: (_e: React.SyntheticEvent, checked: boolean) => {
+                setter(checked);
+              },
+            };
+          },
+          label: 'duplicatable',
+          initialValue: false,
+        },
+        {
+          component: TextField,
+          propFactory: (_s, _r, setter) => {
+            return {
+              fullWidth: true,
+              label: 'Additional Notes',
+              onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
+                setter(event.currentTarget.value);
+              },
+            };
+          },
+          label: 'notes',
+          initialValue: '',
+        },
+      ]}
       dialogStyle={{
         title: 'Add New Item',
         cancelColor: 'secondary',
--- src/components/ContentModal.tsx
+++ src/components/ContentModal.tsx
@@ -1,38 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { KeyboardDatePicker } from '@material-ui/pickers';
-import Checkbox from '@material-ui/core/Checkbox';
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import ConfirmationDialog from './ConfirmationDialog';
 import { fullEvery } from '../utils';
 
 import { DialogStyleProps } from './types';
+import { BaseContent } from '../types';
 
-type ItemDescriptor<T> = {
+type ItemDescriptor<T, K extends keyof T> = {
   component: React.JSXElementConstructor<any>
   propFactory: (
+    state: Partial<T>, // current state
+    reasons: Partial<Record<K,string>>, // current error reasons
     setter: (val: any) => void, // sets item's field in state to value
-    state: Record<string,any>, // current state
-    reasons: Record<string,any>, // current error reasons
+    genericSetter: (
+      label: K,
+      val: any,
+    ) => void, // set any field in state to value
   ) => any
-  label: keyof T
-  initialValue: any
-  validator?: (state: Record<string,any>) => any
+  label: K
+  initialValue: T[K]
+  validator?: (state: Partial<T>) => any
+} | {
+  component?: never
+  propFactory?: never
+  label: K
+  initialValue: T[K]
+  validator?: (state: Partial<T>) => any
 }
 
 type ContentModalProps<T> = {
-  items: ItemDescriptor<T>[]
-  onSubmit: (values?: T) => void
+  items: ItemDescriptor<T, keyof T>[]
+  onSubmit: (values?: T) => void // values is null when submit cancelled
   dialogStyle: DialogStyleProps
   open: boolean
   initialState?: Partial<T>
 }
 
 function ContentModal<
-  T extends Record<string,any>, // type for state
+  T extends BaseContent, // type for state
 >(props: ContentModalProps<T>): React.ReactElement {
   const [state, setState] = React.useState<Partial<T>>({});
   const [
@@ -40,187 +46,9 @@ function ContentModal<
     setReasons,
   ] = React.useState<Partial<Record<keyof T,string>>>({});
 
-  // Default items for content
-  const items: ItemDescriptor<T>[] = [
-    {
-      component: TextField,
-      propFactory: (setter, _state, reasons) => {
-        return {
-          fullWidth: true,
-          label: 'Title',
-          onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
-            setter(event.currentTarget.value);
-          },
-          error: !!reasons['title'],
-          helperText: reasons['title'],
-        };
-      },
-      label: 'title',
-      initialValue: '',
-      validator: (state) => {
-        if (!state['title']) {
-          return 'Title is required.';
-        } else {
-          return null;
-        }
-      },
-    },
-    {
-      component: TextField,
-      propFactory: (setter) => {
-        return {
-          fullWidth: true,
-          label: 'Description',
-          onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
-            setter(event.currentTarget.value);
-          },
-        };
-      },
-      label: 'description',
-      initialValue: '',
-    },
-    {
-      component: (props) => (
-        <>
-          <Button
-            variant={'contained'}
-            component={'label'}
-            onChange={props.onChange}
-          >
-            Upload File
-            <input
-              type={'file'}
-              accept={'*'}
-              hidden
-            />
-          </Button>
-          <Typography>{props.text}</Typography>
-        </>
-      ),
-      propFactory: (setter, state) => {
-        return {
-          onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
-            const target = event.target as HTMLInputElement;
-            setter(target.files?.[0]);
-          },
-          text: state['file'] ?
-            `Existing file: ${state['file'].name}`
-            :
-            'No file chosen',
-        };
-      },
-      label: 'file',
-      initialValue: '',
-    },
-    {
-      component: TextField,
-      propFactory: (setter, _state, reasons) => {
-        return {
-          fullWidth: true,
-          label: 'Year of Publication',
-          onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
-            setter(event.currentTarget.value);
-          },
-          error: !!reasons['year'],
-          helperText: reasons['year'],
-        };
-      },
-      label: 'year',
-      initialValue: '',
-      validator: (state) => {
-        if (!state['year'] || isNaN(Number(state['year']))) {
-          return 'Invalid year';
-        } else {
-          return null;
-        }
-      },
-    },
-    {
-      component: KeyboardDatePicker,
-      propFactory: (setter, state) => {
-        return {
-          disableToolbar: true,
-          variant: 'inline',
-          format: 'MM/dd/yyyy',
-          label: 'Reviewed Date',
-          onChange: (date: Date) => {
-            setter(date);
-          },
-          value: state['reviewDate'],
-        };
-      },
-      label: 'reviewDate',
-      initialValue: new Date(),
-    },
-    {
-      component: TextField,
-      propFactory: (setter) => {
-        return {
-          fullWidth: true,
-          label: 'Copyright Notes',
-          onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
-            setter(event.currentTarget.value);
-          },
-        };
-      },
-      label: 'copyright',
-      initialValue: '',
-    },
-    {
-      component: TextField,
-      propFactory: (setter) => {
-        return {
-          fullWidth: true,
-          label: 'Rights Statement',
-          onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
-            setter(event.currentTarget.value);
-          },
-        };
-      },
-      label: 'rights',
-      initialValue: '',
-    },
-    {
-      component: (props) => {
-        return (
-          <>
-            <Typography>Duplicatable</Typography>
-            <Checkbox {...props} />
-          </>
-        );
-      },
-      propFactory: (setter, state) => {
-        return {
-          checked: state['duplicatable'],
-          onChange: (_e: React.SyntheticEvent, checked: boolean) => {
-            setter(checked);
-          },
-        };
-      },
-      label: 'duplicatable',
-      initialValue: false,
-    },
-    {
-      component: TextField,
-      propFactory: (setter) => {
-        return {
-          fullWidth: true,
-          label: 'Additional Notes',
-          onChange: (event: React.SyntheticEvent<HTMLInputElement>) => {
-            setter(event.currentTarget.value);
-          },
-        };
-      },
-      label: 'notes',
-      initialValue: '',
-    },
-  ];
-
   // Initializes state with initial values and initial state
   React.useEffect(() => {
-    const allItems = items.concat(props.items);
-
-    setState(Object.assign(allItems.reduce<Partial<T>>(
+    setState(Object.assign(props.items.reduce<Partial<T>>(
       (accum, val) => ({
         ...accum,
         [val.label]: val.initialValue,
@@ -239,6 +67,16 @@ function ContentModal<
     },
     [setState],
   );
+
+  const genericSetter = React.useCallback(
+    (name: keyof T, val: any) => {
+      setState(oldState => ({
+        ...oldState,
+        [name]: val,
+      }));
+    },
+    [setState],
+  );
   
   // Performs validation on submission
   const onSubmit = React.useCallback((submitted: boolean) => {
@@ -246,7 +84,7 @@ function ContentModal<
       const reasonDraft: typeof reasons = {};
 
       // Check no reasons present
-      if (fullEvery(items, item => {
+      if (fullEvery(props.items, item => {
         if (item.validator) {
           const reason = item.validator(state);
           reasonDraft[item.label] = reason;
@@ -273,14 +111,15 @@ function ContentModal<
       {...props.dialogStyle}
     >
       <Grid container>
-        {items.concat(props.items).map((item, idx) => {
+        {props.items.map((item, idx) => {
           return (
             <Grid item key={idx} xs={12} style={{ marginBottom: '10px' }} >
-              <item.component {...item.propFactory(
-                stateSetter(item.label),
+              {item.component && <item.component {...item.propFactory(
                 state,
                 reasons,
-              )} />
+                stateSetter(item.label),
+                genericSetter,
+              )} />}
             </Grid>
           );
         })}
