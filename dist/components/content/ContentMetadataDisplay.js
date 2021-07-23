c39a3e6e8d214c25a55517384b0894432bf4f533
Jonathan Zhao <zhaojonathan99@gmail.com>
2021-07-23 15:24:14 -0700

    Test impl of toAdd metadata for tagger

--- dist/components/content/ContentMetadataDisplay.js
+++ dist/components/content/ContentMetadataDisplay.js
@@ -1,4 +1,6 @@
 import { jsx as _jsx } from "react/jsx-runtime";
+//Importing from outside the project
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 //Importing from other files of the projects
 import ContentTagger from './ContentTagger';
@@ -9,9 +11,27 @@ import ContentTagger from './ContentTagger';
  * @returns A display for all content metadata.
  */
 function ContentMetadata(props) {
+    const [metadata, setMetadata] = React.useState(props.metadata);
+    React.useEffect(() => {
+        setMetadata(props.metadata);
+    }, [props.metadata]);
+    React.useEffect(() => {
+        setMetadata(oldState => {
+            const newState = Object.entries(oldState).reduce((accum, [key, val]) => {
+                var _a;
+                return Object.assign(Object.assign({}, accum), { [key]: val.concat((_a = props.toAdd[key]) !== null && _a !== void 0 ? _a : []) });
+            }, {});
+            Object.entries(props.toAdd).forEach(([key, val]) => {
+                if (!(key in newState)) {
+                    newState[key] = val;
+                }
+            });
+            return newState;
+        });
+    }, [props.toAdd]);
     return (_jsx(Grid, Object.assign({ container: true, spacing: props.spacing }, { children: props.metadataTypes.map(metadataType => {
             var _a, _b, _c, _d;
-            return (_jsx(Grid, Object.assign({ item: true, xs: (_a = props.width) !== null && _a !== void 0 ? _a : 12, style: { marginBottom: (_b = props.mb) !== null && _b !== void 0 ? _b : '10px' } }, { children: _jsx(ContentTagger, Object.assign({}, props.actions, { metadataType: metadataType, selected: (_c = props.metadata[metadataType.id]) !== null && _c !== void 0 ? _c : [], options: (_d = props.options[metadataType.id]) !== null && _d !== void 0 ? _d : [], label: metadataType.name }), void 0) }), metadataType.id));
+            return (_jsx(Grid, Object.assign({ item: true, xs: (_a = props.width) !== null && _a !== void 0 ? _a : 12, style: { marginBottom: (_b = props.mb) !== null && _b !== void 0 ? _b : '10px' } }, { children: _jsx(ContentTagger, Object.assign({}, props.actions, { metadataType: metadataType, selected: (_c = metadata[metadataType.id]) !== null && _c !== void 0 ? _c : [], options: (_d = props.options[metadataType.id]) !== null && _d !== void 0 ? _d : [], label: metadataType.name }), void 0) }), metadataType.id));
         }) }), void 0));
 }
 export default ContentMetadata;
--- src/components/content/ContentMetadataDisplay.tsx
+++ src/components/content/ContentMetadataDisplay.tsx
@@ -12,6 +12,7 @@ type ContentMetadataProps<
 > = {
   metadataTypes: T[]
   metadata: Record<number,M[]>
+  toAdd: Record<number, M[]>
   options: Record<number,M[]>
   actions: ContentTaggerActionProps<T,M>
   width?: GridSize
@@ -29,6 +30,31 @@ function ContentMetadata<
   T extends BaseMetadataType,
   M extends BaseMetadata<T>,
 >(props: ContentMetadataProps<T,M>): React.ReactElement {
+  const [metadata, setMetadata] = React.useState(props.metadata);
+
+  React.useEffect(() => {
+    setMetadata(props.metadata);
+  }, [props.metadata]);
+
+  React.useEffect(() => {
+    setMetadata(oldState => {
+      const newState = Object.entries(oldState).reduce((accum, [key, val]) => {
+        return {
+          ...accum,
+          [key]: val.concat(props.toAdd[key as unknown as number] ?? []),
+        };
+      }, {} as Record<number, M[]>);
+
+      Object.entries(props.toAdd).forEach(([key, val]) => {
+        if (!(key in newState)) {
+          newState[key as unknown as number] = val;
+        }
+      });
+
+      return newState;
+    });
+  }, [props.toAdd]);
+
   return (
     <Grid container spacing={props.spacing} >
       {props.metadataTypes.map(metadataType => {
@@ -42,7 +68,7 @@ function ContentMetadata<
             <ContentTagger
               {...props.actions}
               metadataType={metadataType}
-              selected={props.metadata[metadataType.id] ?? []}
+              selected={metadata[metadataType.id] ?? []}
               options={props.options[metadataType.id] ?? []}
               label={metadataType.name}
             />
